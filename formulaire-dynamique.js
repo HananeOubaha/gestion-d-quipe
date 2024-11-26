21750a90ed080272c30c9d43ba2c1a0b85284e21
Hanane Oubaha <Hananeouba03@gmail.com>
2024-11-26 17:02:02 +0100

    j'i pu ajouté les deux fonctions de gestion de pop up openForm et closeForm dans le ficher js

---
+++ formulaire-dynamique.js
@@ -0,0 +1,7 @@
+//  la fonction responsable de l'affichage du pop up d'ajout
+function openForm() {
+    document.getElementById('popup-ajout').classList.remove('hidden');
+  }
+  function closeForm() {
+    document.getElementById('popup-ajout').classList.add('hidden');
+  }
