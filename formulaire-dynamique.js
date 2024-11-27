d89a8ac4233111b78aecaf33c8a382ece5db6999
Hanane Oubaha <Hananeouba03@gmail.com>
2024-11-27 10:22:27 +0100

    j'ai pu récupéré les informations remplie dans les champs de formulaire et je l'ai stocké dans des nouveaux variables de types constantes

--- formulaire-dynamique.js
+++ formulaire-dynamique.js
@@ -5,3 +5,19 @@ function openForm() {
   function closeForm() {
     document.getElementById('popup-ajout').classList.add('hidden');
   }
+
+  function submitPlayerForm(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const picture =document.getElementById('picture').value;
+    const nationality = document.getElementById('nationality').value;
+    const club = document.getElementById('club').value;
+    const rating = document.getElementById('rating').value;
+    const pace = document.getElementById('pace').value;
+    const shooting = document.getElementById('shooting').value;
+    const passing = document.getElementById('passing').value;
+    const dribbling = document.getElementById('dribbling').value;
+    const defending = document.getElementById('defending').value;
+    const physical = document.getElementById('physical').value;
+  }
+  
